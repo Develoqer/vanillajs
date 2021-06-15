e174d65046e932134fcc9b1e94e775e958efff4c
Hamid KEZZAL <hamidkezzal@free.fr>
2021-06-15 21:32:35 +0200

    i have to learn more about css position relative and absolute flexbox or grid to success this work i will finish him later :-) in cha Allah

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const burger = document.querySelectorAll(".toggle-burger")
+console.log(burger[0]);
+
+const menu = document.querySelectorAll(".nav");
+console.log(menu);
+
+const container = document.getElementById("container");
+console.log(container);
